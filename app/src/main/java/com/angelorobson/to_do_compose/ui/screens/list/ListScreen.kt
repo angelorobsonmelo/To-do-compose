5a452998cf8ae830203e0e7af8aab0a7bfc39565
Angelo Melo <angelorobsonmelo@gmail.com>
2022-02-07 14:40:33 +0000

    undo implemented

--- app/src/main/java/com/angelorobson/to_do_compose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/angelorobson/to_do_compose/ui/screens/list/ListScreen.kt
@@ -33,6 +33,9 @@ fun ListScreen(
     DisplaySnackBar(
         scaffoldState = scaffoldState,
         handleDatabaseActions = { sharedViewModel.handleDatabaseActions(action = action) },
+        onUndoClicked = {
+            sharedViewModel.action.value = it
+        },
         taskTitle = sharedViewModel.title.value,
         action = action
     )
@@ -80,6 +83,7 @@ fun ListFab(
 fun DisplaySnackBar(
     scaffoldState: ScaffoldState,
     handleDatabaseActions: () -> Unit,
+    onUndoClicked: (Action) -> Unit,
     taskTitle: String,
     action: Action
 ) {
@@ -92,9 +96,32 @@ fun DisplaySnackBar(
             scope.launch {
                 val snackBarResult = scaffoldState.snackbarHostState.showSnackbar(
                     message = "${action.name}: $taskTitle",
-                    actionLabel = "OK"
+                    actionLabel = setActionLabel(action = action)
+                )
+                undoDeletedTask(
+                    action = action,
+                    snackBarResult = snackBarResult,
+                    onUndoClicked = onUndoClicked
                 )
             }
         }
+    }
+}
+
+private fun setActionLabel(action: Action): String {
+    return if (action.name == Action.DELETE.name) {
+        return Action.UNDO.name
+    } else {
+        "OK"
+    }
+}
+
+private fun undoDeletedTask(
+    action: Action,
+    snackBarResult: SnackbarResult,
+    onUndoClicked: (Action) -> Unit
+) {
+    if (snackBarResult == SnackbarResult.ActionPerformed && action == Action.DELETE) {
+        onUndoClicked(Action.UNDO)
     }
 }
--- app/src/main/java/com/angelorobson/to_do_compose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/angelorobson/to_do_compose/ui/viewmodels/SharedViewModel.kt
@@ -118,7 +118,7 @@ class SharedViewModel @Inject constructor(
 
             }
             Action.UNDO -> {
-
+                addTAsk()
             }
             else -> {
 
